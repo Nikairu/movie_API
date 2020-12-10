278c679aacf94e4c992663f5d7caac67a7238454
Nikairu <diogosilvaoliveirawork@gmail.com>
2020-12-10 11:20:35 +0000

    cors *

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ const Models = require('./models.js');
 let auth = require('./auth')(app);
 
 //let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
-app.options('*', cors());
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization, Content-Length, X-Requested-With'
+  );
+  next();
+};
+
+app.use(allowCrossDomain);
 
 mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
